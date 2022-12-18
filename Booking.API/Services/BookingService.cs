9a4ac0ea159589ea94534cc0c8157221ce4ed545
GiangTonEnlab <giang.ton@enlabsoftware.com>
2022-12-18 12:40:15 +0700

    update

--- Booking.API/Services/BookingService.cs
+++ Booking.API/Services/BookingService.cs
@@ -91,7 +91,6 @@ namespace Booking.API.Services
         {
             var booking = await GetBookingAsync(id);
             var isCanCheckDoneBooking = await _bookingRepository.AnyAsync(_ => _.Id == id
-                                                            && _.DuePayment < DateTime.UtcNow
                                                             && (_.Status == BookingStatus.ExtendDue
                                                                 || _.Status == BookingStatus.DuePayment)
                                                             && !_.IsDelete);
